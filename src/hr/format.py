b5b4513385668be01216c2bfa6a4f12370e10646
damokappa <ddamian.sbp@gmail.com>
2019-05-19 20:55:38 -0700

    Added CSV format

--- src/hr/format.py
+++ src/hr/format.py
@@ -1,7 +1,13 @@
-import json, csv, sys
+import json, csv
 
-def format_json(json_list, outfile=sys.stdout):
+def format_json(json_list, outfile):
     outfile.write(json.dumps(json_list, sort_keys=True, indent=4))
     outfile.close()
 
-def format_csv()
+def format_csv(csv_list, outfile):
+    outfile.write('name,id,home,shell\n')
+    writer = csv.writer(outfile)
+    rows =  [[csv_list['name'], csv_list['id'], csv_list['home'], csv_list['shell']] for user in csv_list]
+
+    writer.writerows(rows)
+    outfile.close()
