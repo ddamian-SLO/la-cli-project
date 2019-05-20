96a87cc8acb9bcfa62994686c0e38e988aaffa57
damokappa <ddamian.sbp@gmail.com>
2019-05-19 21:31:59 -0700

    Fixed for loop to actually pull info.

--- src/hr/export.py
+++ src/hr/export.py
@@ -7,7 +7,9 @@ def format_json(json_list, outfile):
 def format_csv(csv_list, outfile):
     outfile.write('name,id,home,shell\n')
     writer = csv.writer(outfile)
-    rows =  [[csv_list['name'], csv_list['id'], csv_list['home'], csv_list['shell']] for user in csv_list]
 
-    writer.writerows(rows)
+    for user in csv_list:
+        rows =  [[user['name'], user['id'], user['home'], user['shell']]] 
+        writer.writerows(rows)
+
     outfile.close()
