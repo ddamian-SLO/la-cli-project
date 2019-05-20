9454e1eca336289e70b418bbfa26094f70f5f5b0
damokappa <ddamian.sbp@gmail.com>
2019-05-19 20:42:49 -0700

    Created function to get non system users from pwd.

---
+++ src/hr/users.py
@@ -0,0 +1,17 @@
+import pwd
+
+def get_users():
+    valid_users = [0]
+    total_users = pwd.getpwall()
+    for i in range(len(total_users), -1, -1):
+        if total_users[i].pw_uid < 1000:
+            break
+        else:
+            valid_users.append({
+                "name": total_users[i].pw_name,
+                "id": total_users[i].pw_uid,
+                "home": total_users[i].pw_dir,
+                "shell": total_users[i].pw_shell
+            })
+    return valid_users
+        
