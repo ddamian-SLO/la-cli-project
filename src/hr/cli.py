5ec3d0161aea5e29baf92815bcfa4ece5c932df1
damokappa <ddamian.sbp@gmail.com>
2019-05-19 21:10:50 -0700

    Added main to cli function

--- src/hr/cli.py
+++ src/hr/cli.py
@@ -1,16 +1,34 @@
-from argparse import Action, ArgumentParser
+import argparse
 
 def create_parser():
 
-    parser = ArgumentParser(description="""
+    parser = argparse.ArgumentParser(description="""
     Tool used to display user information in CSV or JSON format
     """)
 
     parser.add_argument('--format', 
-    help="Format of output, JSON or CSV",
-    default='json', choices=['json', 'csv']
+    help="Format of output, [json or csv]",
+    default='json', choices=['json', 'csv'],
+    type=str.lower
     )
     parser.add_argument('--path', help="Specify path to export file")
 
     return parser
 
+def main():
+    import sys
+    from hr import users, export
+    from hr import users as u 
+    
+    args = create_parser().parse_args()
+    users = u.get_users()
+    
+    if args.path:
+        output_file = open(args.path, 'w', newline='')
+    else: 
+        output_file = sys.stdout
+
+    if args.format == 'csv':
+        export.format_csv(users, output_file)
+    else:
+        export.format_json(users, output_file)
